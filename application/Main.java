2078385face6caf8a183ebb721772921f717a997
khoa165 <khoatle165@gmail.com>
2019-11-25 19:30:15 -0600

    Set up the project, create gitignore file, add files that need to be ignored.

---
+++ application/Main.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Khoa Thien Le (Harry), Shannon Stiles, Kenneth Mui, Saniya Khullar.
+ *
+ */
+public class Main extends Application {
+
+  @Override
+  public void start(Stage arg0) throws Exception {
+    // TODO Auto-generated method stub
+
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
