96e56f4249bd2d8b6a54ce9cb92abf01439824d4
shanstiles <sstiles@wisc.edu>
2019-11-29 17:35:24 -0600

    Add User name and friend list to the User class - created getters and one add friend method

--- application/User.java
+++ application/User.java
@@ -1,5 +1,35 @@
 package application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class User {
 
+	private String name;
+	private Set<String> friendNames;
+	private Set<User> friends;
+
+	public User(String name) {
+		this.name = name;
+		this.friends = new HashSet<User>();
+		this.friendNames = new HashSet<String>();
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public Set<User> getFriends() {
+		return this.friends;
+	}
+
+	public boolean addFriend(User newFriend) {
+		if (this.friendNames.contains(newFriend.getName())) {
+			return false;
+		} else {
+			this.friendNames.add(newFriend.getName());
+			this.friends.add(newFriend);
+			return true;
+		}
+	}
 }
