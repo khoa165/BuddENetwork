674ce198471c3d676898431aae96bb9f7c3bace9
khullarama <skhullar2@wisc.edu>
2019-12-01 21:21:06 -0600

    Added in the Class File Header

--- application/Main.java
+++ application/Main.java
@@ -1,3 +1,47 @@
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//
+// Title: Main.java
+// Files: Main.java   (A-Team 12 Milestone 2)
+// Course: CS 400, Section 001/002, Fall 2019
+//
+// Authors: Khoa Thien Le (Harry), Shannon Stiles, Kenneth Mui, Saniya Khullar
+// Email: ktle4@wisc.edu, sstiles@wisc.edu, klmui@wisc.edu, skhullar2@wisc.edu
+// Lecturer's Name: Deb Deppeler
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
+//
+// Partner Name: NONE
+// Partner Email: NONE
+// Lecturer's Name: Deb Deppeler
+//
+// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
+// _X__ Write-up states that pair programming is allowed for this assignment.
+// _X__ We have both read and understand the course Pair Programming Policy.
+// _X__ We have registered our team prior to the team registration deadline.
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully
+// acknowledge and credit those sources of help here. Instructors and TAs do
+// not need to be credited here, but tutors, friends, relatives, room mates
+// strangers, etc do. If you received no outside help from either type of
+// source, then please explicitly indicate NONE.
+//
+// Persons: CS Learning Center
+// Online Sources: NONE
+//
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
+
+/**
+ * Please note that this class is A Team 12's implementation of the JavaFX 
+ * Project, which is the BuddE Network.  
+ * 
+ * @author Khoa Thien Le (Harry), Shannon Stiles, Kenneth Mui, Saniya Khullar,
+ *         Professor Deppeler, Teaching Assistants
+ *
+ */
+
+
 package application;
 
 import java.util.List;
@@ -35,195 +79,190 @@ import javafx.stage.Stage;
  *
  */
 public class Main extends Application {
-	// store any command-line arguments that were entered.
-	// NOTE: this.getParameters().getRaw() will get these also
-	private List<String> args;
+  // store any command-line arguments that were entered.
+  // NOTE: this.getParameters().getRaw() will get these also
+  private List<String> args;
 
-	private static final int WINDOW_WIDTH = 800;
-	private static final int WINDOW_HEIGHT = 400;
-	private static final int CANVAS_WIDTH = 400;
-	private static final int CANVAS_HEIGHT = 200;
-	private static final String APP_TITLE = "BuddE Network";
+  private static final int WINDOW_WIDTH = 800;
+  private static final int WINDOW_HEIGHT = 400;
+  private static final int CANVAS_WIDTH = 400;
+  private static final int CANVAS_HEIGHT = 200;
+  private static final String APP_TITLE = "BuddE Network";
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// save args example
-		args = this.getParameters().getRaw();
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    // save args example
+    args = this.getParameters().getRaw();
 
-		// Create a vertical box with Hello labels for each args
-		VBox centerVBox = new VBox();
+    // Create a vertical box with Hello labels for each args
+    VBox centerVBox = new VBox();
 
-		// Add interactive graph to center pane
-		// Creates a canvas that can draw shapes and text
-		Canvas canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-		// Write some text
-		// Text is filled with the fill color
-		gc.setFill(Color.GREEN);
-		gc.setFont(new Font(30));
-		gc.fillText("Hello World!", 70, 170);
-		// Draw a line
-		// Lines use the stroke color
-		gc.setStroke(Color.BLUE);
-		gc.setLineWidth(2);
-		gc.strokeLine(40, 100, 250, 50);
-		// Draw a few circles
-		gc.setFill(Color.BLACK);
-		// The circles draw from the top left, so to center them, subtract the
-		// radius from each coordinate
-		gc.fillOval(40 - 15, 100 - 15, 30, 30);
-		gc.setFill(Color.RED);
-		gc.fillOval(250 - 15, 50 - 15, 30, 30);
+    // Add interactive graph to center pane
+    // Creates a canvas that can draw shapes and text
+    Canvas canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    GraphicsContext gc = canvas.getGraphicsContext2D();
+    // Write some text
+    // Text is filled with the fill color
+    gc.setFill(Color.GREEN);
+    gc.setFont(new Font(30));
+    gc.fillText("Hello World!", 70, 170);
+    // Draw a line
+    // Lines use the stroke color
+    gc.setStroke(Color.BLUE);
+    gc.setLineWidth(2);
+    gc.strokeLine(40, 100, 250, 50);
+    // Draw a few circles
+    gc.setFill(Color.BLACK);
+    // The circles draw from the top left, so to center them, subtract the
+    // radius from each coordinate
+    gc.fillOval(40 - 15, 100 - 15, 30, 30);
+    gc.setFill(Color.RED);
+    gc.fillOval(250 - 15, 50 - 15, 30, 30);
 
-		centerVBox.getChildren().add(canvas);
+    centerVBox.getChildren().add(canvas);
 
-		// Main layout is Border Pane example (top,left,center,right,bottom)
-		BorderPane root = new BorderPane();
+    // Main layout is Border Pane example (top,left,center,right,bottom)
+    BorderPane root = new BorderPane();
 
-		// Add title to top of the root pane
-		root.setTop(new Label(APP_TITLE));
+    // Add title to top of the root pane
+    root.setTop(new Label(APP_TITLE));
 
-		// Add the vertical box to the center of the root pane
-		root.setCenter(centerVBox);
+    // Add the vertical box to the center of the root pane
+    root.setCenter(centerVBox);
 
-		// Add ComboBox to left pane
-		ComboBox<String> comBox = new ComboBox<String>();
-		comBox.getItems().addAll("Harry", "Kenny", "Saniya", "Shannon");
-		root.setLeft(comBox);
+    // Add ComboBox to left pane
+    ComboBox<String> comBox = new ComboBox<String>();
+    comBox.getItems().addAll("Harry", "Kenny", "Saniya", "Shannon");
+    root.setLeft(comBox);
 
-		// create hbox for add new user
-		HBox addUserHBox = new HBox();
-		Button newUser = new Button("Create New User");
-		TextField newUserName = new TextField();
-		newUserName.setPromptText("Please Enter a Name");
-		addUserHBox.getChildren().addAll(newUser, newUserName);
-		addUserHBox.setSpacing(10);
+    // create hbox for add new user
+    HBox addUserHBox = new HBox();
+    Button newUser = new Button("Create New User");
+    TextField newUserName = new TextField();
+    newUserName.setPromptText("Please Enter a Name");
+    addUserHBox.getChildren().addAll(newUser, newUserName);
+    addUserHBox.setSpacing(10);
 
-		// create hbox for removing user
-		HBox removeUserHBox = new HBox();
-		Button removeUser = new Button("Remove User");
-		// List<String> allUsersList = List.of("Harry", "Kenny", "Saniya",
-		// "Shannon");
-		// ComboBox<String> removeUserName = new
-		// ComboBox<String>(FXCollections.observableList(allUsersList));
-		TextField removeUserName = new TextField();
-		removeUserName.setPromptText("Please Enter a Name");
-		removeUserHBox.getChildren().addAll(removeUser, removeUserName);
-		removeUserHBox.setSpacing(29);
+    // create hbox for removing user
+    HBox removeUserHBox = new HBox();
+    Button removeUser = new Button("Remove User");
+    // List<String> allUsersList = List.of("Harry", "Kenny", "Saniya",
+    // "Shannon");
+    // ComboBox<String> removeUserName = new
+    // ComboBox<String>(FXCollections.observableList(allUsersList));
+    TextField removeUserName = new TextField();
+    removeUserName.setPromptText("Please Enter a Name");
+    removeUserHBox.getChildren().addAll(removeUser, removeUserName);
+    removeUserHBox.setSpacing(29);
 
-		// create vbox for user functions
-		VBox userVBox = new VBox();
-		userVBox.getChildren().addAll(addUserHBox, removeUserHBox);
-		userVBox.setSpacing(10);
-		
-	      // create separator between user and buddE functions
-        Separator separator1 = new Separator();
-        // separator1.setMaxWidth(150);
-		
-		// create hbox for adding BuddE connection between central user and
-		// other user
-		HBox addBuddEHBox = new HBox();
-		// Label addBuddECentralUser = new Label("Harry");
-		Button addBuddE = new Button("Add BuddE");
-		TextField addBuddEOtherUser = new TextField();
-		addBuddEOtherUser.setPromptText("Please Enter a Name");
-		addBuddEHBox.getChildren().addAll(addBuddE, addBuddEOtherUser);
-		addBuddEHBox.setSpacing(31);
+    // create vbox for user functions
+    VBox userVBox = new VBox();
+    userVBox.getChildren().addAll(addUserHBox, removeUserHBox);
+    userVBox.setSpacing(10);
 
-		// create hbox for removing BuddE connection between central user and
-		// other user
-		HBox removeBuddEHBox = new HBox();
-		// Label removeBuddECentralUser = new Label("Harry");
-		Button removeBuddE = new Button("Remove BuddE");
-		TextField removeBuddEOtherUser = new TextField();
-		removeBuddEOtherUser.setPromptText("Please Enter a Name");
-		removeBuddEHBox.getChildren().addAll(removeBuddE, removeBuddEOtherUser);
-		removeBuddEHBox.setSpacing(10);
+    // create separator between user and buddE functions
+    Separator separator1 = new Separator();
+    // separator1.setMaxWidth(150);
 
-		// create vbox for BuddE functions
-		VBox buddEVBox = new VBox();
-		buddEVBox.getChildren().addAll(addBuddEHBox, removeBuddEHBox);
-		buddEVBox.setSpacing(10);
+    // create hbox for adding BuddE connection between central user and
+    // other user
+    HBox addBuddEHBox = new HBox();
+    // Label addBuddECentralUser = new Label("Harry");
+    Button addBuddE = new Button("Add BuddE");
+    TextField addBuddEOtherUser = new TextField();
+    addBuddEOtherUser.setPromptText("Please Enter a Name");
+    addBuddEHBox.getChildren().addAll(addBuddE, addBuddEOtherUser);
+    addBuddEHBox.setSpacing(31);
 
-		
-        // create separator between buddE functions and mutual BuddEs/friends
-        Separator separator2 = new Separator();
-		
-        
-        
-        
-        
-		
-		// ---------------------- Mutual BuddEs code ------------------------------
-        ListView<String> list = new ListView<>();
-        ObservableList<String> mutualFriends = FXCollections
-                .observableList(List.of("Saniya", "Shannon"));
+    // create hbox for removing BuddE connection between central user and
+    // other user
+    HBox removeBuddEHBox = new HBox();
+    // Label removeBuddECentralUser = new Label("Harry");
+    Button removeBuddE = new Button("Remove BuddE");
+    TextField removeBuddEOtherUser = new TextField();
+    removeBuddEOtherUser.setPromptText("Please Enter a Name");
+    removeBuddEHBox.getChildren().addAll(removeBuddE, removeBuddEOtherUser);
+    removeBuddEHBox.setSpacing(10);
 
-        VBox mutualBuddEsVBox = new VBox();
-        
-        Label mutualBuddEsTitle = new Label("Mutual BuddEs");
-        
-        Scene scene = new Scene(mutualBuddEsVBox, 100, 100);
-        // stage.setScene(scene);
-        // stage.setTitle("Mutual BuddEs");
-        mutualBuddEsVBox.getChildren().addAll(list);
-        VBox.setVgrow(list, Priority.ALWAYS);
-
-        list.setItems(mutualFriends);
-
-        list.setCellFactory((ListView<String> l) -> new addFriendToCell());
-
-        //stage.show();
-        
-        
-        // ------------------End Mutual BuddEs code -------------------------------
-        
-        
-        
-		// Add vbox with hboxes in it to right pane
-		VBox rightVBox = new VBox();
-		rightVBox.getChildren().addAll(userVBox, separator1, buddEVBox, separator2, mutualBuddEsTitle, mutualBuddEsVBox);
-		//rightVBox.getChildren().addAll(userVBox, separator1, buddEVBox, separator2);
-		rightVBox.setSpacing(20);
-
-		// set right pane
-		root.setRight(rightVBox);
+    // create vbox for BuddE functions
+    VBox buddEVBox = new VBox();
+    buddEVBox.getChildren().addAll(addBuddEHBox, removeBuddEHBox);
+    buddEVBox.setSpacing(10);
 
 
-		// Add done button to bottom pane
-		Text status = new Text("Status: ");
-		root.setBottom(status);
-		Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+    // create separator between buddE functions and mutual BuddEs/friends
+    Separator separator2 = new Separator();
 
-		// Add the stuff and set the primary stage
-		primaryStage.setTitle(APP_TITLE);
-		primaryStage.setScene(mainScene);
-		primaryStage.show();
-	}
-	
-	/**
-	 * Please note that this class allows us to view the list of mutual BuddEs,
-	 * between the central user and one of his/her buddEs.  
-	 * @author 
-	 *
-	 */
-	static class addFriendToCell extends ListCell<String> {
-      @Override
-      public void updateItem(String item, boolean empty) {
-          super.updateItem(item, empty);
-          Label friendNameLabel;
-          if (item != null) {
-              friendNameLabel = new Label(item);
-              setGraphic(friendNameLabel);
-          }
-      }
+
+    // ---------------------- Mutual BuddEs code ------------------------------
+    ListView<String> list = new ListView<>();
+    ObservableList<String> mutualFriends =
+        FXCollections.observableList(List.of("Saniya", "Shannon"));
+
+    VBox mutualBuddEsVBox = new VBox();
+
+    Label mutualBuddEsTitle = new Label("Mutual BuddEs");
+
+    Scene scene = new Scene(mutualBuddEsVBox, 100, 100);
+    // stage.setScene(scene);
+    // stage.setTitle("Mutual BuddEs");
+    mutualBuddEsVBox.getChildren().addAll(list);
+    VBox.setVgrow(list, Priority.ALWAYS);
+
+    list.setItems(mutualFriends);
+
+    list.setCellFactory((ListView<String> l) -> new addFriendToCell());
+
+    // stage.show();
+
+    // ------------------End Mutual BuddEs code -------------------------------
+
+    // Add vbox with hboxes in it to right pane
+    VBox rightVBox = new VBox();
+    rightVBox.getChildren().addAll(userVBox, separator1, buddEVBox, separator2,
+        mutualBuddEsTitle, mutualBuddEsVBox);
+    // rightVBox.getChildren().addAll(userVBox, separator1, buddEVBox, separator2);
+    rightVBox.setSpacing(20);
+
+    // set right pane
+    root.setRight(rightVBox);
+
+
+    // Add done button to bottom pane
+    Text status = new Text("Status: ");
+    root.setBottom(status);
+    Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    // Add the stuff and set the primary stage
+    primaryStage.setTitle(APP_TITLE);
+    primaryStage.setScene(mainScene);
+    primaryStage.show();
   }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		launch(args);
-	}
-	
+  /**
+   * Please note that this class allows us to view the list of mutual BuddEs, between the central user
+   * and one of his/her buddEs.
+   * 
+   * @author
+   *
+   */
+  static class addFriendToCell extends ListCell<String> {
+    @Override
+    public void updateItem(String item, boolean empty) {
+      super.updateItem(item, empty);
+      Label friendNameLabel;
+      if (item != null) {
+        friendNameLabel = new Label(item);
+        setGraphic(friendNameLabel);
+      }
+    }
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+
 }
